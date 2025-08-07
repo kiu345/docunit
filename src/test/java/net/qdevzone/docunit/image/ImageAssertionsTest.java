c7f3698113f9ffd317b541cc654cd2ee85e21acd
S. Daurer <s.daurer@q-spot.org>
2025-08-07 23:52:56 +0200

    Initial license header added

--- src/test/java/net/qdevzone/docunit/image/ImageAssertionsTest.java
+++ src/test/java/net/qdevzone/docunit/image/ImageAssertionsTest.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2025-2025 the original author or authors
+ *
+ * See the README file(s) distributed with this work for additional information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package net.qdevzone.docunit.image;
 
 import static org.assertj.core.api.Assertions.assertThat;
